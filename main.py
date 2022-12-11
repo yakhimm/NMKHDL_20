df958e103a4c9d96120d09ea2ef7b16aee4af6cc
yakhimm <20120307@student.hcmus.edu.vn>
2022-12-10 22:23:22 -0500

    models kmeans

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
 import pandas as pd
 import numpy as np
+from datetime import date
 from gets import get_weather_air
 
 #Tạo list chứa danh sách url
-lst = np.genfromtxt('url.csv', dtype = 'str')
+lst = np.genfromtxt('./data/url.csv', dtype = 'str')
 lst = lst[1:]
 
 df_list = []
@@ -11,9 +12,10 @@ df_list = []
 for i in lst:
     dict_value = get_weather_air(i)
     series_value = pd.Series(dict_value)
-    
     df_list.append(series_value)
     
-df = pd.concat(df_list, axis=1).T 
+df = pd.concat(df_list, axis = 1).T 
 
-df.to_csv('list.csv', index=False)
+today = date.today()
+d = today.strftime("%b-%d-%Y")
+df.to_csv(f'./data/data/{d}.csv', index = False)
